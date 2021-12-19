a7720c73a9691e97fc26516a9905f30bc368152d
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-12-19 23:26:53 +0100

    Style Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,15 @@
 export default function Footer() {
   return (
-    <footer style={{ backgroundColor: "aqua" }}>
-      <div className="copyright">
-        <p>&copy; 2021 {"\u2022"} JustVegan</p>
+    <footer className="footer">
+      <div className="container">
+        <div className="copyright">
+          <p>&copy; 2021 {"\u2022"} JustVegan</p>
+        </div>
+        <div className="disclaimer">
+          Disclaimer: This website was build as an educational and
+          self-promoting material, and does not offer any actual services.
+        </div>
       </div>
-      <p>
-        Disclaimer: This website was build as an educational and self-promoting
-        material, and does not offer any actual services.
-      </p>
     </footer>
   );
 }
